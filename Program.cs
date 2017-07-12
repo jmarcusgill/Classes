ce62d5b1ce762a0257b00d9b4c3f0efbc67951f9
jmarcusgill <jmarcusgill@gmail.com>
2017-07-12 15:38:20 -0500

    finished

--- Program.cs
+++ Program.cs
@@ -7,9 +7,21 @@ namespace classes
     {
         static void Main(string[] args)
         {
+            // contructor method for creating new company:
+            Company newCompany = new Company ("ComicGetIt", DateTime.Now);
+
+            // create instances for new employees
+            Employee newEmp1 = new Employee ("Marcus", "CEO/ Full-Stack", DateTime.Now);
+            Employee newEmp2 = new Employee ("Krissy", "Front-End", DateTime.Now);
+            Employee newEmp3 = new Employee ("Mitchell", "Back-End", DateTime.Now);
            
+            // referencing public class Company, calling add methods I created
+            newCompany.addEmployee(newEmp1);
+            newCompany.addEmployee(newEmp2);
+            newCompany.addEmployee(newEmp3);
 
-
+            // referencing public class Company, calling list method I created
+            newCompany.ListEmployees();
 
 
         }
@@ -43,9 +55,34 @@ namespace classes
 
             The constructor will set the value of the public properties
         */
+        public Company (string nameInput, DateTime dateInput) 
+        {
+            Name = nameInput;
+            CreatedOn = dateInput;
+        }
+        public void ListEmployees() 
+        {
+            foreach (Employee each in this.employeeList) {
+                Console.WriteLine($"{each.EmployeeName}: {each.JobTitle}");
+            }
+
+        }
     }
 
-    public class Employee {
+    // Create a class that gets and sets employee data
+    public class Employee 
+    {
+        public string EmployeeName { get; set; }
+        public string JobTitle { get; set; }
+        public DateTime startDate { get; set; }
+
+        public Employee (string name, string title, DateTime date)
+        {
+            EmployeeName = name;
+            JobTitle = title;
+            startDate = date;
+        }
+
 
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-Setup
+\# Classes
 
+## Setup
+
+```
 mkdir -p ~/workspace/csharp/exercises/classes && cd $_
 dotnet new console
 dotnet restore
-Instructions
+```
 
-Copy this Company class into your Program.cs file.
+## Instructions
+
+1. Copy this `Company` class into your `Program.cs` file.
+
+```cs
 public class Company
 {
     /*
@@ -28,7 +35,8 @@ public class Company
         The constructor will set the value of the public properties
     */
 }
-Create a class that contains information about employees of a company and define methods to get/set employee name, job title, and start date.
-Consider the concept of aggregation, and modify the Company class so that you assign employees to a company.
-In the Main method, create a company, and three employees, and then assign the employees to the company.
-Update the Company class to write the name of each employee to the console. Consider a method named ListEmployees().
+```
+1. Create a class that contains information about employees of a company and define methods to get/set employee name, job title, and start date.
+1. Consider the concept of [aggregation](../08_RELATIONSHIPS.md#aggregation), and modify the `Company` class so that you assign employees to a company. 
+1. In the `Main` method, create a company, and three employees, and then assign the employees to the company.
+1. Update the `Company` class to write the name of each employee to the console. Consider a method named `ListEmployees()`.
